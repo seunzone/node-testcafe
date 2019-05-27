1c519ef15e59e6d843e18b5b18ff1e424591ace9
seunzone <darealseun@gmail.com>
2019-05-27 22:24:10 +0100

    test new todo

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,19 +1,47 @@
-import { Selector } from 'testcafe';
+import { Selector, ClientFunction } from 'testcafe';
 
+const title = Selector('h4');
+const addTodoButton = Selector('a.btn.btn-primary');
+const getLocation = ClientFunction(() => document.location.href)
 
-// selectors
-const title = Selector('h1');
-const par = Selector('p');
 
 fixture('Test')
   .page('http://localhost:5000');
 
-test('Test', async (t) => {
-  // check title, add job button, table rows, and job exists
+test('Test_Landing_Page', async (t) => {
   await t
-    .expect(title.innerText).eql('Hello Express')
-    await t
-.debug()
-    .expect(par.innerText).eql('Hello Express')
-
+    .expect(title.innerText).eql('My Todo List')
+    .expect(addTodoButton.innerText).eql('Add ToDo')
 });
+
+test('New_Todo', async (t) => {
+    await t
+    .click(addTodoButton) 
+    // test that new route is /new
+    .expect(getLocation()).match(/.*\/new*/)
+    // fill form on the new page
+    await t
+    .typeText('input[name="title"]', 'Eat Food')
+    .typeText('input[name="time"]', '3pm')
+    .click(Selector('button[type="submit"]'))
+    // expect the action to direct back to the home route
+    .expect(getLocation()).match(/.*\/*/)
+});
+
+// the method below makes use of the DOM selectors
+test.skip('New_Todo_Second_Method', async (t) => {
+    await t
+    .click(Selector('body > div > div.row.area > div.col-md-8 > div > a'))
+    // test that new route is /new
+    .expect(getLocation()).match(/.*\/new*/)
+    // fill form on the new page
+    await t
+    .typeText(Selector('body > div > div > div.col-md-8 > div > form > div:nth-child(1) > input'), 
+    'Eat Food', { replace: true })
+    .typeText(Selector('body > div > div > div.col-md-8 > div > form > div:nth-child(2) > input'), 
+    '3pm', { replace: true })
+    .click(Selector('body > div > div > div.col-md-8 > div > form > button'))
+    // expect the action to direct back to the home route
+    .expect(getLocation()).match(/.*\/*/)
+});
+
